3d30585b42ea2106b66d810b95a77a13349aa9c6
20123 <20123@DESKTOP-998UHVO>
2020-12-08 12:29:32 -0400

    Se agrego la ventana que se encarga de
     gestionar las solicitudes de empresa.

--- Proyecto Final/src/Visual/OfertasEmpleo.java
+++ Proyecto Final/src/Visual/OfertasEmpleo.java
@@ -10,13 +10,28 @@ import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import Logico.CrucialWork;
+import Logico.Obrer0;
+import Logico.SolicitudDeEmpresa;
+import Logico.SolicitudEmpleo;
+import Logico.Tecnic0;
+import Logico.Universitario;
+
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class OfertasEmpleo extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
 	private JTable tbOfertas;
+	public static DefaultTableModel modelo;
+	public static Object[] fila;
+	private String temp;
+
 
 	/**
 	 * Launch the application.
@@ -52,8 +67,47 @@ public class OfertasEmpleo extends JDialog {
 		scrollPane.setBounds(10, 53, 565, 489);
 		contentPanel.add(scrollPane);
 		
+		modelo = new DefaultTableModel();
+		String[] columns = {"RNC", "Tipo de empleado", "Salaro Minimo","Disp. viaje", "Estado de solicitud"};
+		modelo.setColumnIdentifiers(columns);
 		tbOfertas = new JTable();
+		tbOfertas.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int seleccion = tbOfertas.getSelectedRow();
+				if(seleccion != -1) {
+					
+					temp = (String) modelo.getValueAt(seleccion, 0);
+				}else {
+					
+				}
+			}
+		});
+		
 		scrollPane.setViewportView(tbOfertas);
+		CargarTabla();
 		
 	}
+	private void CargarTabla() {
+		modelo.setRowCount(0);
+		fila = new Object[modelo.getColumnCount()];
+		for(SolicitudDeEmpresa e : CrucialWork.getInstance().getSde()) {
+			if(e != null) {
+				fila[0] = e.getId();
+				fila[1]= e.getTipoDeEmpleado();
+				fila[2] = e.getSalarioMinimo();
+				if(e.isDispViaje() != false) {
+					fila[3] = "Sí";
+				}else fila[3] = "No";
+				if(e.isEstado() != false) {
+					fila[4] = "Aceptado";
+				}else fila[4] = "Pendiente";
+				modelo.addRow(fila);
+			}
+		}
+		
+		
+		
+	
+}
 }
